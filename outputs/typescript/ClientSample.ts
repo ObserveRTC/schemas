09a49868222251c13828e897ddda52d0fcd73d15
balazskreith <kreith.balazs@gmail.com>
2025-10-21 13:48:48 +0300

    feat: add 'frozen' state to IceCandidatePairStats and update related schemas

--- outputs/typescript/ClientSample.ts
+++ outputs/typescript/ClientSample.ts
@@ -165,7 +165,7 @@ export type IceCandidatePairStats = {
 	*/
 	remoteCandidateId?: string;
 
-	state?: "new" | "in-progress" | "waiting" | "failed" | "succeeded" | "cancelled" | "inprogress";
+	state?: "new" | "frozen" | "in-progress" | "waiting" | "failed" | "succeeded" | "cancelled" | "inprogress";
 	/**
 	* Whether this candidate pair has been nominated.
 	*/
